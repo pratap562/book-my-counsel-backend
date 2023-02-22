3a8a68323dce079ad793db764f27b1850288c49f
pratap562 <kumpratap562@gmail.com>
2023-02-22 11:54:18 +0530

    models updated

--- Models/Advocate.Model.js
+++ Models/Advocate.Model.js
@@ -1,20 +1,23 @@
 const mongoose = require('mongoose')
 
 const AdvocateModel = mongoose.model('advocate', mongoose.Schema({
-    user_id: { type: String, require: true },
+    user_id: { type: String, required: true },
     zoom_id: { type: String, required: true },
     name: { type: String, required: true },
-    picture: { type: String, require: true },
-    role_title: { type: String, require: true },
-    location: { type: String, require: true },
-    pricing: { type: Number, require: true },
-    total_Earnings: { type: Number, require: true },
-    total_worked_hour: { type: Number, require: true },
-    short_description: { type: String, require: true },
-    long_description: { type: String, require: true },
-    short_description: { type: String, require: true },
-    fluent_languate: { type: Array, require: true },
-    conversational_languate: { type: Array, require: true }
+    picture: { type: String, required: true },
+    role_title: { type: String, required: true },
+    location: { type: String, required: true },
+    pricing: { type: Number, required: true },
+    total_Earnings: { type: Number, required: true },
+    total_worked_hour: { type: Number, required: true },
+    short_description: { type: String, required: true },
+    long_description: { type: String, required: true },
+    short_description: { type: String, required: true },
+    total_jobs: { type: Number, default:10},
+    fluent_languate: { type: Array, required: true },
+    conversational_languate: { type: Array, required: true },
+    skills:{type:Array,required:true},
+    Education:{type:Array,required:true}
 }))
 
 module.exports = AdvocateModel
