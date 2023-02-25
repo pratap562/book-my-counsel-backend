f7cbac02c29ccec9857cf277d9f0f77b54460df6
pratap562 <kumpratap562@gmail.com>
2023-02-25 21:35:12 +0530

    slote booking apis

--- routes/Slotes/slote.js
+++ routes/Slotes/slote.js
@@ -44,8 +44,11 @@ formattedDate = ar[1] + '-' + ar[0] + '-' + ar[2]
 
 const formattedTime = currentDate.toLocaleTimeString('en-US', timeOptions);
 
+
+// user will ask for this
 slot.get('/:advocateId', async (req, res) => {
     const { advocateId } = req.params
+    console.log(advocateId, 'aid')
 
     console.log(formattedDate, formattedTime)
     try {
@@ -87,8 +90,53 @@ slot.get('/:advocateId', async (req, res) => {
     }
 })
 
+// one advocate will ask for ther slots
+// slot.get('/my/all', () => { console.log('hiiiiiiiiiiiiiiiiiiiiii') }, authenticate, autharize(['advocate']), async (req, res) => {
+//     const { advocateId } = req.body
 
-slot.patch('/:slotId', authenticate, async (req, res) => {
+//     console.log(formattedDate, formattedTime)
+//     try {
+//         let result = await slotModel.find({
+//             $and: [
+//                 { advocate_id: advocateId }, // check if advocateId exists
+//                 { date: { $gt: formattedDate } }, // check if the date is greater than the current date
+//                 {
+//                     $or: [
+//                         { date: formattedDate, time: { $gt: formattedTime } }, // check if the time is greater than the current time if the date is the same
+//                         { date: { $gt: formattedDate } } // check if the date is greater than the current date if the time is in the past
+//                     ]
+//                 }
+//             ]
+//         }).sort({ date: 1, time: -1 })
+
+//         let lastDate = result[0].date || ''
+//         let weekSlots = []
+//         let daySlots = []
+//         result.forEach((doc) => {
+//             if (doc.date == lastDate) {
+//                 daySlots.push(doc)
+//             } else {
+//                 if (daySlots.length >= 1) {
+//                     weekSlots.push(daySlots)
+//                 }
+//                 lastDate = doc.date
+//                 daySlots = [doc]
+//             }
+//         })
+//         if (daySlots.length >= 1) {
+//             weekSlots.push(daySlots)
+//         }
+//         res.status(200).send({ "data": weekSlots })
+//     } catch (err) {
+//         res.status(500).send({ "err": "try after some time" })
+//         console.log(err)
+//     }
+// })
+
+
+
+
+slot.patch('/book/:slotId', authenticate, async (req, res) => {
     let { user_id } = req.body
     let { slotId } = req.params
     try {
