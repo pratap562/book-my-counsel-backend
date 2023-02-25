3bee55311cfae5b8321f849f8d79a2682ebad35b
pratap562 <kumpratap562@gmail.com>
2023-02-25 20:09:25 +0530

    advocate can create slots and admin can verify delete advocates

--- Models/Slot.Model.js
+++ Models/Slot.Model.js
@@ -1,10 +1,11 @@
-const mongoose = require('slot')
+const mongoose = require('mongoose')
 
-const slot = mongoose.model('user', mongoose.Schema({
+const slotModel = mongoose.model('slot', mongoose.Schema({
     advocate_id: { type: String, required: true },
     client_id: { type: String, required: true },
     date: { type: String, required: true },
+    time: { type: String, required: true },
     canceled: { type: Boolean, default: false }
 }))
 
-module.exports = slot
+module.exports = slotModel
