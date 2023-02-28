4c0fabe023279a2f50a5ebb44dea4587455cf483
pratap562 <kumpratap562@gmail.com>
2023-03-01 01:04:40 +0530

    updated cors

--- server.js
+++ server.js
@@ -26,10 +26,7 @@ const port = 3200
 //     credentials: true
 // }))
 // app.use(cors())
-app.use(cors({
-    origin: [process.env.NEXT_URL, 'https://checkout.stripe.com'],
-    credentials: true
-}));
+app.use(cors());
 // httpserver.use(cors)
 app.use(express.json())
 app.use(cookieParser())
